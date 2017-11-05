98edafa7a7bdb0135937eac77c3d9402ff2a7790
mark <mark.kolovics@gmail.com>
2017-11-05 10:21:05 +0100

    For end-to-end cases the console messages has been suppressed.

--- src/test/java/com/mark/sudokuchecker/EndToEndTest.java
+++ src/test/java/com/mark/sudokuchecker/EndToEndTest.java
@@ -2,6 +2,10 @@ package com.mark.sudokuchecker;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.ExpectedSystemExit;
@@ -15,6 +19,18 @@ import org.junit.rules.ExpectedException;
  */
 public class EndToEndTest {
 	String path = "src/test/resources/";
+	
+	/**
+	 * Suppress console messages for these tests. 
+	 */
+	@Before
+	public void beforeAll () {
+	    System.setOut(new PrintStream(new OutputStream() {
+            public void write(int b) {
+                //DO NOTHING
+            }
+        }));
+	}
 
 	@Rule
 	public final ExpectedSystemExit exit = ExpectedSystemExit.none();
